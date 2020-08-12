8b2f53d21a26b56fb9d502aed33a4a849f949b54
Aaron Wislang <asw101@users.noreply.github.com>
2020-08-11 23:56:20 -0400

    Update cloud-init.sh

--- 200800-cloud-init/cloud-init.sh
+++ 200800-cloud-init/cloud-init.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-echo "hello, world" | tee /tmp/output.txt
+git clone -b asw101-dev https://github.com/asw101/Moodle
+cd /home/azureadmin/Moodle/scripts/
+apt-get update && apt-get install -y moreutils
+
+# setup_webserver
+bash setup_webserver.sh /home/azureadmin/env.json 2>&1 | ts '%Y%m%d-%H:%M:%S' | tee setup_webserver.log
