432eb9707de3f69bc36318e6a63582bfb196d190
Aaron Wislang <asw101@users.noreply.github.com>
2020-08-28 16:50:59 -0400

    Update cloud-init.sh

--- 200800-cloud-init/moodle1/cloud-init.sh
+++ 200800-cloud-init/moodle1/cloud-init.sh
@@ -8,3 +8,7 @@ apt-get update && apt-get install -y moreutils
 
 # setup_webserver
 bash setup_webserver.sh /home/azureuser/env.json 2>&1 | ts '%Y%m%d-%H:%M:%S' | tee setup_webserver.log
+
+MOODLE_SITEURL=$(cat env.json | jq -r '.siteProfile.siteURL')
+sed -i "/wwwroot/c\$CFG->wwwroot = 'https://${MOODLE_SITEURL}';" /var/www/html/moodle/config.php
+service nginx restart
