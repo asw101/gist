8ba55113c99353175b309ab4bc36bc7eab0f0489
Aaron Wislang <asw101@users.noreply.github.com>
2020-09-02 18:01:17 -0400

    Update cloud-init.sh

--- 200800-cloud-init/share1/cloud-init.sh
+++ 200800-cloud-init/share1/cloud-init.sh
@@ -3,5 +3,26 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
+sudo apt-get install -y \
+        mosh
+
+sudo tee -a /home/azureuser/.bash_aliases > /dev/null <<'EOT'
+function d-run-dev {
+        docker run --rm \
+                -v ~/.ssh/:/root/.ssh/ \
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                -v ${PWD}:/pwd/ \
+                -w /pwd/ \
+                --name 'dev' \
+                -it aaronmsft/dev
+}
+
+function d-exec-dev {
+        docker exec -it dev bash
+}
+EOT
+
+docker pull aaronmsft/dev
+
 [[ -z "${HELLO:-}" ]] && HELLO='...'
 echo "HELLO: ${HELLO}" | tee output.txt
